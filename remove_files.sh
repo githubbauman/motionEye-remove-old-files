45fb347644e6dc5d83eb36246d26b2f64b97988e
Arthur Bauman <github@bauman.nu>
2021-03-31 15:07:56 +0200

    Create remove_files.sh

---
+++ remove_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK=/dev/mmcblk0p3
+ALERT=80
+MOTIONDATA=/data/output
+
+
+function usage {
+    echo $(df -h "${DISK}" | tail -1 | awk '{ gsub("%","",$5); print $5 }')
+}
+
+while [ $(usage) -ge ${ALERT} ]
+    do
+        oldest_file=$(ls -ltd $(find "${MOTIONDATA}" -not -path '*/\.*' -type f \( ! -iname ".*" \) ) | tail -1 | awk '{ gsub("%","",$5); print $9 }')
+        $(rm  $oldest_file)
+    done
